aeb9f3f84fd740290c7229dd605fb1239aed9303
Neisha <neisha@me.com>
2018-12-02 17:28:59 +0800

    Prototype complete

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
 import MovieList from './MovieList.js'
+import Spinner from './Spinner.js'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: false,
       movies: [],
       errorMsg: '',
     };
   }
 
-  componentWillMount() {
-    getMoviesFromApiAsync()
-      .then((movies) => {
-          this.setState({
-            movies: movies,
-            errorMsg: '',
-          });        
-      })
-      .catch((error) => {
-        this.setState({
-          movies: [],
-          errorMsg: 'Loading movies failed. Please try again later.'
-        });
+  handleClick = () => {
+    // TODO: disable button when loading
+    this.setState(
+      {
+        loading: true,
+        movies: [],
+        errorMsg: '',
       });
+    setTimeout(() => { // TODO: remove debug code
+      getMoviesFromApiAsync()
+        .then((movies) => {
+            this.setState({
+              movies: movies,
+              errorMsg: '',
+              loading: false,
+            });
+        })
+        .catch((error) => {
+          this.setState({
+            movies: [],
+            errorMsg: 'Loading movies failed. Please try again later.',
+            loading: false,
+          });
+        });
+    }, 1000);
   }
 
   render() {
     return (
       <div className="App">
         <h1>Movies to expand the mind</h1>
+        <button onClick={this.handleClick}>Load Movies</button>
         {
+          this.state.loading ? 
+          <Spinner /> :
           !this.state.errorMsg ? 
           <MovieList movies={this.state.movies} /> :
           <div className="error">{this.state.errorMsg}</div>
